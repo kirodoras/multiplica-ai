211f8d2f5e973a8b9df291ac0157599a0a19a8c8
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2023-01-10 15:58:51 -0300

    exercise done

--- README.md
+++ README.md
@@ -6,4 +6,12 @@
 
 ### Enunciado:
 
-Escreva um código que deve imprimir a tabela de multiplicação desse número fornecido, e deve exibir um cabeçalho com o nome Tabela de multiplicação de X, onde X é o número a ser multiplicado.
+Crie utilizando tudo que aprendeu até o momento, uma pirâmide de números, onde você deve imprimir em cada linha separada a quantidade de números que está sendo iterado no momento! Por exemplo, se a sua iteração for de 1 a 5 ficaria assim o resultado final:
+
+```java
+1
+22
+333
+4444
+55555
+```
--- src/App.java
+++ src/App.java
@@ -3,22 +3,29 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Digite um número inteiro: ");
-            try {
-                int numero = scanner.nextInt();
-                System.out.println("Tabela de multiplicação de " + numero);
-                for (int i = 1; i <= 10; i++) {
-                    System.out.println(numero
-                            + " x "
-                            + i
-                            + " = "
-                            + (numero * i));
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("O valor informado não é um número!");
+            System.out.print("Digite o primeiro número inteiro: ");
+            int num1 = scanner.nextInt();
+            System.out.println("Num1: " + num1);
+            System.out.print("Digite o segundo número inteiro: ");
+            int num2 = scanner.nextInt();
+            System.out.println("Num2: " + num2);
+
+            if (num2 < num1) {
+                int aux = num1;
+                num1 = num2;
+                num2 = aux;
             }
+            
+            for (int i = num1; i <= num2; i++) {
+                String numbers = "";
+                for (int j = 1; j <= i; j++) {
+                    numbers += i;
+                }
+                System.out.println(numbers);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("O valor informado não é um número!");
         }
     }
 }
